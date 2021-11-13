a61a715399ac63ac387a5c0bdb4a5894c91f97a8
pereiramor <morgana.pereirasc@gmail.com>
2021-11-13 10:34:43 -0300

    alteraçoes projeto morgana

--- src/main/java/com/betha/projeto/projeto/ProjetoApplication.java
+++ src/main/java/com/betha/projeto/projeto/ProjetoApplication.java
@@ -2,6 +2,10 @@ package com.betha.projeto.projeto;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class ProjetoApplication {
@@ -9,5 +13,15 @@ public class ProjetoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetoApplication.class, args);
 	}
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurerAdapter() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedMethods("*").allowedOrigins("*");
+			}
+		};
+	}
+
 
 }
